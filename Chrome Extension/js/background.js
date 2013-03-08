019f46abc0865dabcd14d5861cee1fbec74dd4c1
Bacra Woo <wf@tianqu.com.cn>
2013-03-08 17:25:42 +0800

    Chrome：征集插件对获得信息的处理

--- Chrome Extension/js/background.js
+++ Chrome Extension/js/background.js
@@ -15,7 +15,7 @@ chrome.pageAction.onClicked.addListener(function(tab){
 	if (url.indexOf('file://') > -1){
 		url = url.substr(8);
 		initWS(function(){
-			_WS.send('D:/Projects'+url.substr(2, url.lastIndexOf('/') -2));
+			_WS.send('D:/Projects' + url.substr(2, url.lastIndexOf('/') -2));
 		});
 		chrome.tabs.update(tab.id, {'url': url.replace(/^\w:\//, 'http://www.test.com/').replace(/#.*/g, '')});
 	} else {
@@ -24,7 +24,8 @@ chrome.pageAction.onClicked.addListener(function(tab){
 });
 
 
-var _WS;
+var _WS, _WS_SetTimeout4less = false, _WS_SetTimeout4reload = false,
+	_WS_Interval = 1000;
 function initWS(callback){
 	if (!_WS) {
 		_WS = new WebSocket('ws://127.0.0.1:8080/');
@@ -38,8 +39,29 @@ function initWS(callback){
 		};
 		_WS.onmessage = function (evt) {
 			var json = JSON.parse(evt.data);
-			console.log(json);
-			console.log('Retrieved data from server: ' + evt.data);
+			if (json.filename.indexOf('.less') != -1) {
+				if (!_WS_SetTimeout4less) {
+					_WS_SetTimeout4less = true;
+					chrome.tabs.executeScript(null, {
+						'code': ''
+					});
+
+					setTimeout(function(){
+						_WS_SetTimeout4less = false;
+					}, _WS_Interval);
+				}
+			} else {
+				if (!_WS_SetTimeout4reload) {
+					_WS_SetTimeout4reload = true;
+					_WS_SetTimeout4less = true;
+					chrome.tabs.reload();
+
+					setTimeout(function(){
+						_WS_SetTimeout4reload = false;
+						_WS_SetTimeout4less = false;
+					}, _WS_Interval);
+				}
+			}
 		};
 		_WS.onerror = function (evt) { 
 			_WS = false;
