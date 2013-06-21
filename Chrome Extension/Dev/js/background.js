7c40d356d1246d63ef9f6cba24bd03253d368280
Bacra Woo <Bacra.Woo@gmail.com>
2013-06-21 13:43:06 +0800

    删除dev插件

--- Chrome Extension/Dev/js/background.js
+++
@@ -1,7 +0,0 @@
-// 按钮事件
-var id = 'bkpcdngelgpfbpjlaioplcophekpdomc';
-chrome.browserAction.onClicked.addListener(function(){
-	chrome.management.setEnabled(id, false, function() {
-		chrome.management.setEnabled(id, true);
-	});
-});
